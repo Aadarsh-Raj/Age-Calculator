b10388ca2b8e8ed053096680dad304fa2ee88b7a
Aadarsh Raj <aadarshraj06062001@gmail.com>
2023-11-03 13:18:43 +0530

    fixed non-existing values

--- index-script.js
+++ index-script.js
@@ -1,39 +1,54 @@
-// document.getElementById("calculateAgeButton").addEventListener("click", calculateAge);
 function calculateAge() {
     const birthdate = new Date(document.getElementById("birthdate").value);
     const currentDate = new Date();
-
-    const years = currentDate.getFullYear() - birthdate.getFullYear();
-    const months = currentDate.getMonth() - birthdate.getMonth();
-    const days = currentDate.getDate() - birthdate.getDate();
-
+    if (birthdate > currentDate) {
+      alert("Please enter a valid Date of Birth");
+  return;
+    }
+    let years = currentDate.getFullYear() - birthdate.getFullYear();
+    let months = currentDate.getMonth() - birthdate.getMonth();
+    let days = currentDate.getDate() - birthdate.getDate();
+  
     if (months < 0 || (months === 0 && days < 0)) {
-        years--;
+      years--;
     }
-
+  
     if (months < 0) {
-        months += 12;
+      months += 12;
     }
-
+  
     if (days < 0) {
-        const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-        days += lastDayOfMonth;
-        months--;
+      const lastDayOfMonth = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0
+      ).getDate();
+      days += lastDayOfMonth;
+      months--;
     }
 
-    const age = `${years} years, ${months} months, and ${days} days`;
-    document.getElementById("output").textContent = age;
-   if(years <= 16){
-        document.getElementById("teen").style.display ="block";
-        document.getElementById("adult").style.display= "none";
-        document.getElementById("old").style.display= "none";
-   }else if(years > 16 && years <=26){
-        document.getElementById("adult").style.display= "block";
-        document.getElementById("teen").style.display= "none";
-        document.getElementById("old").style.display= "none";
-   } else if(years > 26){
+  // const age = `${years} years, ${months} months, and ${days} days`;
+
+  const newYear = document.getElementById("years");
+  const newMonth = document.getElementById("months");
+  const newDay = document.getElementById("days");
+  newYear.innerText = years;
+  newMonth.innerText = months;
+  newDay.innerText = days;
+  if (years <= 16) {
+    document.getElementById("teen").style.display = "block";
+    document.getElementById("adult").style.display = "none";
+    document.getElementById("old").style.display = "none";
+  } else if (years > 16 && years <= 26) {
+    document.getElementById("adult").style.display = "block";
+    document.getElementById("teen").style.display = "none";
+    document.getElementById("old").style.display = "none";
+  } else if (years > 26) {
     document.getElementById("old").style.display = "block";
-    document.getElementById("adult").style.display= "none";
-    document.getElementById("teen").style.display= "none";
-   }
+    document.getElementById("adult").style.display = "none";
+    document.getElementById("teen").style.display = "none";
+  }
 }
+// const reset = document.getElementById("reset-button");
+// console.log(reset);
+
